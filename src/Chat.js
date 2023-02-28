fe8798279706d19b62b192f1eb5d0fe04aaef543
ortizfram <ortizfranco48@gmail.com>
2023-02-28 13:02:13 -0300

    chat__header style

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import './Chat.css'
+import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
+import { Avatar, IconButton } from '@mui/material';
+import React from 'react';
+import './Chat.css';
 
 function Chat() {
   return (
     <div className='chat'>
-      <h1>Im chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen ....</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton> 
+          <IconButton>
+            <AttachFile />  
+          </IconButton> 
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
