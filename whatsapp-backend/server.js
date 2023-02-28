bf2c23abaa0195eeedb0f02dbbdd66707332e4da
melon <ortizfranco48@gmail.com>
2023-02-28 17:39:42 -0300

    api routing app config importing an d listener BACKEND

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,22 @@
+// import
+import express from 'express'
+
+// app config
+//      app instance
+//      port that the app is gonna run
+const app=express()
+const port = process.env.PORT || 9000
+
+// middleware
+
+// DB config
+
+// ???
+
+// api routes
+//      call the api and nothing at the end
+//      in server world 200 means OK
+app.get('/',(req,res)=>res.status(200).send('hello world'))
+
+// listen
+app.listen(port,()=>console.log('Listening on localhost: ${port}'))
