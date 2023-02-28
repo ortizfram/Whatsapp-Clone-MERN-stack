3fc41d8a2acbfe9f6b328247a4a121af1f5d9488
ortizfram <ortizfranco48@gmail.com>
2023-02-28 09:24:38 -0300

    sidebar header style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Sidebar.css';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { IconButton,Avatar } from '@mui/material';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1>Im sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://static.euronews.com/articles/stories/07/23/85/78/1440x810_cmsv2_653ff2f4-3b92-5efa-941c-2ed0b4c50d10-7238578.jpg"/>
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
