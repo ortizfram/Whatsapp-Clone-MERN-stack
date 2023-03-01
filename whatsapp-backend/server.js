c93362ee195502cd60d92df272ee7b76b1dbc79f
ortizfram <ortizfranco48@gmail.com>
2023-03-01 09:44:44 -0300

    troubleshooting API request in postman

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,22 +1,30 @@
+// BUILD an API -------------------------------------------
+
 // import
-import express from 'express'
+import express from 'express';
+
 
 // app config
 //      app instance
 //      port that the app is gonna run
-const app=express()
-const port = process.env.PORT || 9000
+const app = express();
+const port = process.env.PORT || 9000;
+
 
 // middleware
 
+
 // DB config
+
 
 // ???
 
+
 // api routes
 //      call the api and nothing at the end
-//      in server world 200 means OK
-app.get('/',(req,res)=>res.status(200).send('hello world'))
+//      in server world 200 means OK, 201 means createed
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
 
 // listen
-app.listen(port,()=>console.log('Listening on localhost: ${port}'))
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
